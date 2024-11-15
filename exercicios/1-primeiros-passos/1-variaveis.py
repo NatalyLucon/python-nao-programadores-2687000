463065f4d43eb2c67f5a4715b9926abedf711829
NatalyLucon <64039700+NatalyLucon@users.noreply.github.com>
2024-11-15 16:04:20 +0000

    Aplicação de variaveis

--- exercicios/1-primeiros-passos/1-variaveis.py
+++ exercicios/1-primeiros-passos/1-variaveis.py
@@ -9,3 +9,19 @@
 
 
 # Declare uma variável do tipo booleano
+Texto_simples = 'Aplicação de nome'
+Texto_duplo = "Aplicação de nome"
+Texto_triplo = '''Para um texto com varias linhas.'''
+
+Numero_tipo_int = 6
+Numero_tipo_float = 6.3
+
+Booleano = False
+
+print(Texto_simples)
+print(Texto_duplo)
+print(Texto_triplo)
+print(Numero_tipo_int)
+print(Numero_tipo_float)
+print(Booleano)
+
