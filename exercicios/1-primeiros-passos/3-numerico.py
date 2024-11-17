51951017896c2d9b322cec62002f1c04dc4b85ca
NatalyLucon <64039700+NatalyLucon@users.noreply.github.com>
2024-11-17 12:25:11 +0000

    Aplicação numerico

--- exercicios/1-primeiros-passos/3-numerico.py
+++ exercicios/1-primeiros-passos/3-numerico.py
@@ -1,11 +1,13 @@
 data_nascimento = '05-10-1976'
 idade_numerica = 46
 altura = 1.74
-
 # Descubra o tipo de dado de cada variável acima
-
-
+print(type(data_nascimento))
+print(type(idade_numerica))
+print(type(altura))
 # Realize uma operação entre dados do tipo string e inteiro
-
-
+String_inteiro = data_nascimento * idade_numerica
+print(String_inteiro)
 # Realize uma operação entre dados do tipo inteiro e float
+Interio_float = idade_numerica/altura
+print(Interio_float)
