6c1c851f2ccfbf979cc7b669fc5c2a21f0b82ac3
NatalyLucon <64039700+NatalyLucon@users.noreply.github.com>
2024-11-25 23:17:28 +0000

    Loop e range

--- exercicios/3-estruturas-de-controle/2-for-loop.py
+++ exercicios/3-estruturas-de-controle/2-for-loop.py
@@ -1,9 +1,10 @@
 # Crie uma lista
-
+personagens = ('Bill Chipher', 'Mabel', 'Dipper', 'Stanley', 'Wendy', 'Robbie', 'Soos', 'Ford')
 # Crie um for loop para imprimir cada elemento dessa lista
-
-
+for Gravity in personagens:
+  print(Gravity)
 # Crie um objeto iterável utilizando a função range()
-
-
+valor = range(0,21,2)
 # Use esse objeto iterável para criar um for loop que imprima na tela a frase "Estou aprendendo uma linguagem de programação."
+for item in range(0,21,2):
+  print( f'{item} - Estou aprendendo uma linguagem de programação. ')
