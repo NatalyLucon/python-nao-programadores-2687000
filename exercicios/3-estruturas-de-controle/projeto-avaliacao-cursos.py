e07618eee0f9a62252c39b29e73dc4b2ebdb671c
NatalyLucon <64039700+NatalyLucon@users.noreply.github.com>
2024-11-25 22:35:47 +0000

    Projeto de avaliação de curso

--- exercicios/3-estruturas-de-controle/projeto-avaliacao-cursos.py
+++ exercicios/3-estruturas-de-controle/projeto-avaliacao-cursos.py
@@ -5,3 +5,22 @@
 # 4. Crie uma estrutura condicional para verificar se cada variável está contida na lista
 # 5. Se o curso estiver na lista, solicite uma nota para avaliação
 # 6. Armazene essa nota no dicionário, sendo a chave o título do curso e o valor a nota
+cursos = ('Python','Canvas','Ingles','Programação','Excel')
+curso_python = 'Python'
+curso_canvas = 'Canvas'
+curso_ingles = 'Ingles'
+curso_excel = 'Excel'
+nota = {}
+pergunta = input('Qual o seu curso: ')
+if pergunta in curso_python:
+  nota[curso_python] = int(input(f'Qual nota você da ao curso {curso_python}: '))
+if pergunta in curso_canvas:
+  nota[curso_canvas] = int(input(f'Qual nota você da ao curso {curso_canvas}: '))
+if pergunta in curso_ingles:
+  nota[curso_ingles] = int(input(f'Qual nota você da ao curso {curso_ingles}: '))
+if pergunta in curso_excel:
+  nota[curso_excel] = int(input(f'Qual nota você da ao curso {curso_excel}: '))
+else:
+  print(f'Não temos esse curso na lista.')
+print(nota)
+
